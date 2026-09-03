0598123b375f38f662f2d473aeb4f3503fd1b2df
zenith16f <fguzmanc2100@alumno.ipn.mx>
2026-09-03 09:57:42 -0600

    Added small changes on Simbolo and Transicion files, check the logic

--- NFAVisual/src/main/java/automaton/Simbolo.java
+++ NFAVisual/src/main/java/automaton/Simbolo.java
@@ -1,4 +1,20 @@
 package automaton;
 
-public class Simbolo {
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Set;
+
+public final class Simbolo {
+    public static final char EPSILON = 'ε';
+    // You can add more constants here
+
+    private static final Set<Character> ESPECIALES = Set.of(EPSILON);
+
+    // Constructors
+    private Simbolo() {
+    }
+
+    // Methods
+    public static boolean IsSpecial(char simbolo) {
+        return ESPECIALES.contains(simbolo);
+    }
 }
--- NFAVisual/src/main/java/automaton/Transicion.java
+++ NFAVisual/src/main/java/automaton/Transicion.java
@@ -1,4 +1,44 @@
 package automaton;
 
 public class Transicion {
+    private char simboloInferior;
+    private char simboloSuperior;
+    private Estado estadoDestino;
+
+    // Constructors
+    public Transicion(char simbolo, Estado estadoDestino) {
+        this(simbolo, simbolo, estadoDestino);
+    }
+
+    public Transicion(char simboloInferior, char simboloSuperior, Estado estadoDestino) {
+        this.simboloInferior = simboloInferior;
+        this.simboloSuperior = simboloSuperior;
+        this.estadoDestino = estadoDestino;
+    }
+
+    // Getters & Setters
+
+    public char getSimboloInferior() {
+        return simboloInferior;
+    }
+
+    public void setSimboloInferior(char simboloInferior) {
+        this.simboloInferior = simboloInferior;
+    }
+
+    public char getSimboloSuperior() {
+        return simboloSuperior;
+    }
+
+    public void setSimboloSuperior(char simboloSuperior) {
+        this.simboloSuperior = simboloSuperior;
+    }
+
+    public Estado getEstadoDestino() {
+        return estadoDestino;
+    }
+
+    public void setEstadoDestino(Estado estadoDestino) {
+        this.estadoDestino = estadoDestino;
+    }
 }
